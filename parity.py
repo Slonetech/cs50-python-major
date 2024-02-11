9e60f20d29bd0ac3012ea95f14c8943bec54fa97
Slonetech <kengeregordon@gmail.com>
2024-02-12 00:24:54 +0300

    That's dope

--- parity.py
+++ parity.py
@@ -6,9 +6,5 @@ def main():
         print("r is odd")
 
 def is_even(n):
-    if n % 2 == 0:
-        return True
-    else:
-        return False
-    
+    return True if n % 2 == 0 else False
 main()
